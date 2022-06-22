0558ef2e8390282f56adba8f4c05d68ed0a508d2
Mahmoud Shukri <88428407+mahmoudshukri@users.noreply.github.com>
2022-06-22 23:14:18 +0200

    THIRD_COMMIT

--- src/sig/controller/headerTable.java
+++ src/sig/controller/headerTable.java
@@ -1,17 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package sig.controller;
-
-import java.awt.*;
-
-
-/**
- *
- * @author mshukri
- */
-class headerTable extends Component {
-
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package sig.controller;
+//
+//import java.awt.*;
+//
+//
+///**
+// *
+// * @author mshukri
+// */
+//class headerTable extends Component {
+//
+//}
